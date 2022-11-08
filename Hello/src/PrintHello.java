b0e658b9264605c3ec4714d83afd6502e399a1e3
JatinGupta1810 <jatingupta3556@gmail.com>
2022-11-09 02:24:26 +0530

    First Commit

---
+++ Hello/src/PrintHello.java
@@ -0,0 +1,12 @@
+
+public class PrintHello {
+
+	public static void main(String[] args) {
+		
+		int _a$h = 9;
+		System.out.println(_a$h);
+		System.out.println("Hello World in main method");
+		System.out.println("Hello World");
+	}
+
+}
